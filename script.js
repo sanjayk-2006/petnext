e2eac388166309fa3c0ada91bc4696d35e6a1104
sanjayk-2006 <sanjaigaming1010@gmail.com>
2026-09-15 13:18:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,902 @@
+// =====================================================
+// PET DATA
+// =====================================================
+
+const pets = [
+
+    // ================= DOGS =================
+
+    {
+        id: 1,
+        name: "Labrador Retriever",
+        category: "dog",
+        price: 8500,
+        image: "images/Labrador.jpg"
+    },
+
+    {
+        id: 2,
+        name: "Golden Retriever",
+        category: "dog",
+        price: 14500,
+        image: "https://images.unsplash.com/photo-1558788353-f76d92427f16"
+    },
+
+    {
+        id: 3,
+        name: "German Shepherd",
+        category: "dog",
+        price: 12000,
+        image: "images/german.jpg"
+    },
+
+    {
+        id: 4,
+        name: "Beagle",
+        category: "dog",
+        price: 7500,
+        image: "images/beagle.jpg"
+    },
+
+    {
+        id: 5,
+        name: "Pomeranian",
+        category: "dog",
+        price: 9000,
+        image: "images/pomeranian.jpg"
+    },
+
+    {
+        id: 6,
+        name: "Shih Tzu",
+        category: "dog",
+        price: 8500,
+        image: "images/shihtzu.jpg"
+    },
+
+    {
+        id: 7,
+        name: "Pug",
+        category: "dog",
+        price: 7000,
+        image: "images/pug.jpg"
+    },
+
+    {
+        id: 8,
+        name: "Dachshund",
+        category: "dog",
+        price: 6500,
+        image: "images/dachund.jpg"
+    },
+
+    {
+        id: 9,
+        name: "Chihuahua",
+        category: "dog",
+        price: 6000,
+        image: "images/chihuahua.jpg"
+    },
+
+    {
+        id: 10,
+        name: "Cocker Spaniel",
+        category: "dog",
+        price: 8000,
+        image: "images/cocker.jpg"
+    },
+
+    {
+        id: 11,
+        name: "French Bulldog",
+        category: "dog",
+        price: 15000,
+        image: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee"
+    },
+
+    {
+        id: 12,
+        name: "Siberian Husky",
+        category: "dog",
+        price: 14000,
+        image: "https://images.unsplash.com/photo-1605568427561-40dd23c2acea"
+    },
+
+    {
+        id: 13,
+        name: "Border Collie",
+        category: "dog",
+        price: 11000,
+        image: "images/border.jpg"
+    },
+
+    {
+        id: 14,
+        name: "Indian Pariah",
+        category: "dog",
+        price: 4500,
+        image: "https://images.unsplash.com/photo-1543466835-00a7907e9de1"
+    },
+
+    {
+        id: 15,
+        name: "Rajapalayam",
+        category: "dog",
+        price: 10000,
+        image: "images/rajapalayam.jpg"
+    },
+
+
+    // ================= CATS =================
+
+    {
+        id: 16,
+        name: "Persian Cat",
+        category: "cat",
+        price: 6500,
+        image: "images/persian.jpg"
+    },
+
+    {
+        id: 17,
+        name: "British Shorthair",
+        category: "cat",
+        price: 8500,
+        image: "images/britisg.jpg"
+    },
+
+    {
+        id: 18,
+        name: "Siamese Cat",
+        category: "cat",
+        price: 7000,
+        image: "images/siamese.jpg"
+    },
+
+    {
+        id: 19,
+        name: "Maine Coon",
+        category: "cat",
+        price: 12000,
+        image: "images/maine.jpg"
+    },
+
+    {
+        id: 20,
+        name: "Ragdoll Cat",
+        category: "cat",
+        price: 10000,
+        image: "images/ragdoll.jpg"
+    },
+
+    {
+        id: 21,
+        name: "Bengal Cat",
+        category: "cat",
+        price: 9500,
+        image: "images/bengal.jpg"
+    },
+
+    {
+        id: 22,
+        name: "Himalayan Cat",
+        category: "cat",
+        price: 7500,
+        image: "images/himalayan.jpg"
+    },
+
+    {
+        id: 23,
+        name: "Scottish Fold",
+        category: "cat",
+        price: 9000,
+        image: "images/scottish.jpg"
+    },
+
+    {
+        id: 24,
+        name: "American Shorthair",
+        category: "cat",
+        price: 6500,
+        image: "images/american.jpg"
+    },
+
+    {
+        id: 25,
+        name: "Indie Cat",
+        category: "cat",
+        price: 3000,
+        image: "images/indie.jpg"
+    },
+
+
+    // ================= RABBITS =================
+
+    {
+        id: 26,
+        name: "Holland Lop",
+        category: "rabbit",
+        price: 2500,
+        image: "images/holland.jpg"
+    },
+
+    {
+        id: 27,
+        name: "Netherland Dwarf",
+        category: "rabbit",
+        price: 3000,
+        image: "images/netherland.jpg"
+    },
+
+    {
+        id: 28,
+        name: "Mini Rex",
+        category: "rabbit",
+        price: 2800,
+        image: "images/mini.jpg"
+    },
+
+    {
+        id: 29,
+        name: "Lionhead Rabbit",
+        category: "rabbit",
+        price: 3500,
+        image: "images/lionhead.jpg"
+    },
+
+    {
+        id: 30,
+        name: "Mini Lop",
+        category: "rabbit",
+        price: 3000,
+        image: "images/mini.jpg"
+    },
+
+
+    // ================= HAMSTERS =================
+
+    {
+        id: 31,
+        name: "Syrian Hamster",
+        category: "hamster",
+        price: 1200,
+        image: "https://images.unsplash.com/photo-1425082661705-1834bfd09dca"
+    },
+
+    {
+        id: 32,
+        name: "Roborovski Hamster",
+        category: "hamster",
+        price: 1500,
+        image: "images/roborovski.jpg"
+    },
+
+    {
+        id: 33,
+        name: "Campbell Dwarf Hamster",
+        category: "hamster",
+        price: 1400,
+        image: "images/campbell.jpg"
+    },
+
+    {
+        id: 34,
+        name: "Winter White Hamster",
+        category: "hamster",
+        price: 1600,
+        image: "images/winter.jpg"
+    },
+
+
+    // ================= GUINEA PIGS =================
+
+    {
+        id: 35,
+        name: "American Guinea Pig",
+        category: "guinea",
+        price: 1800,
+        image: "images/american.jpg"
+    },
+
+    {
+        id: 36,
+        name: "Abyssinian Guinea Pig",
+        category: "guinea",
+        price: 2200,
+        image: "images/abyssinian.jpg"
+    },
+
+    {
+        id: 37,
+        name: "Peruvian Guinea Pig",
+        category: "guinea",
+        price: 2500,
+        image: "images/peruvian.jpg"
+    },
+
+
+    // ================= BIRDS =================
+
+    {
+        id: 38,
+        name: "Blue Budgerigar",
+        category: "bird",
+        price: 1200,
+        image: "images/blue.jpg"
+    },
+
+    {
+        id: 39,
+        name: "Green Budgerigar",
+        category: "bird",
+        price: 1200,
+        image: "images/greenbudgerigar.jpg"
+    },
+
+    {
+        id: 40,
+        name: "Lutino Budgerigar",
+        category: "bird",
+        price: 1500,
+        image: "images/lutino.jpg"
+    },
+
+    {
+        id: 41,
+        name: "Grey Cockatiel",
+        category: "bird",
+        price: 3500,
+        image: "images/greycockatiel.jpg"
+    },
+
+    {
+        id: 42,
+        name: "Lutino Cockatiel",
+        category: "bird",
+        price: 4000,
+        image: "images/lutinocockatiel.jpg"
+    },
+
+    {
+        id: 43,
+        name: "Peach Faced Lovebird",
+        category: "bird",
+        price: 2500,
+        image: "images/peach.jpg"
+    },
+
+    {
+        id: 44,
+        name: "Yellow Canary",
+        category: "bird",
+        price: 2200,
+        image: "images/yellowcanary.jpg"
+    },
+
+
+    // ================= FISH =================
+
+    {
+        id: 45,
+        name: "Goldfish",
+        category: "fish",
+        price: 300,
+        image: "images/goldfish.jpg"
+    },
+
+    {
+        id: 46,
+        name: "Guppy",
+        category: "fish",
+        price: 200,
+        image: "images/guppy.jpg"
+    },
+
+    {
+        id: 47,
+        name: "Betta Fish",
+        category: "fish",
+        price: 500,
+        image: "images/betta.jpg"
+    },
+
+    {
+        id: 48,
+        name: "Molly Fish",
+        category: "fish",
+        price: 250,
+        image: "images/mollyfish.jpg"
+    },
+
+    {
+        id: 49,
+        name: "Platy Fish",
+        category: "fish",
+        price: 250,
+        image: "images/platy.jpg"
+    },
+
+    {
+        id: 50,
+        name: "Tetra Fish",
+        category: "fish",
+        price: 300,
+        image: "images/tetra.jpg"
+    },
+
+    {
+        id: 51,
+        name: "Angelfish",
+        category: "fish",
+        price: 700,
+        image: "images/angelfish.jpg"
+    },
+
+    {
+        id: 52,
+        name: "Koi Fish",
+        category: "fish",
+        price: 1000,
+        image: "images/koifish.jpg"
+    }
+
+];
+
+
+// =====================================================
+// CART
+// =====================================================
+
+let cart = [];
+
+
+// =====================================================
+// DISPLAY PETS
+// =====================================================
+
+function displayPets(list) {
+
+    const petGrid = document.getElementById("petGrid");
+    const noResults = document.getElementById("noResults");
+    const resultCount = document.getElementById("resultCount");
+
+    petGrid.innerHTML = "";
+
+    resultCount.textContent = `${list.length} pets found`;
+
+    if (list.length === 0) {
+        noResults.style.display = "block";
+        return;
+    }
+
+    noResults.style.display = "none";
+
+    list.forEach(pet => {
+
+        const card = document.createElement("div");
+
+        card.className = "pet-card";
+
+        card.innerHTML = `
+
+            <div class="pet-image">
+
+                <img
+                    src="${pet.image}?auto=format&fit=crop&w=600&q=80"
+                    alt="${pet.name}"
+                >
+
+                <span class="category-badge">
+                    ${pet.category.toUpperCase()}
+                </span>
+
+            </div>
+
+            <div class="pet-info">
+
+                <h3>${pet.name}</h3>
+
+                <p class="pet-category">
+                    ${pet.category}
+                </p>
+
+                <h4>
+                    ₹${pet.price.toLocaleString("en-IN")}
+                </h4>
+
+                <button
+                    class="add-cart"
+                    onclick="addPetToCart(${pet.id})">
+
+                    🛒 Add to Cart
+
+                </button>
+
+            </div>
+        `;
+
+        petGrid.appendChild(card);
+
+    });
+}
+
+
+// =====================================================
+// SEARCH + FILTER
+// =====================================================
+
+function searchPets() {
+
+    const searchInput =
+        document.getElementById("searchInput")
+        .value
+        .toLowerCase()
+        .trim();
+
+    const category =
+        document.getElementById("categoryFilter").value;
+
+    const price =
+        document.getElementById("priceFilter").value;
+
+
+    const filteredPets = pets.filter(pet => {
+
+        const searchMatch =
+            pet.name.toLowerCase().includes(searchInput) ||
+            pet.category.toLowerCase().includes(searchInput);
+
+
+        const categoryMatch =
+            category === "all" ||
+            pet.category === category;
+
+
+        let priceMatch = true;
+
+
+        if (price === "low") {
+            priceMatch = pet.price < 5000;
+        }
+
+        if (price === "medium") {
+            priceMatch =
+                pet.price >= 5000 &&
+                pet.price <= 15000;
+        }
+
+        if (price === "high") {
+            priceMatch = pet.price > 15000;
+        }
+
+
+        return (
+            searchMatch &&
+            categoryMatch &&
+            priceMatch
+        );
+
+    });
+
+
+    displayPets(filteredPets);
+}
+
+
+// =====================================================
+// FILTER
+// =====================================================
+
+function filterPets() {
+    searchPets();
+}
+
+
+// =====================================================
+// ADD TO CART
+// =====================================================
+
+function addPetToCart(id) {
+
+    const pet = pets.find(item => item.id === id);
+
+    if (!pet) {
+        return;
+    }
+
+
+    const existingPet =
+        cart.find(item => item.id === id);
+
+
+    if (existingPet) {
+
+        existingPet.quantity++;
+
+    } else {
+
+        cart.push({
+            ...pet,
+            quantity: 1
+        });
+
+    }
+
+
+    updateCart();
+
+    alert(`${pet.name} added to cart!`);
+}
+
+
+// =====================================================
+// UPDATE CART
+// =====================================================
+
+function updateCart() {
+
+    const cartCount =
+        document.getElementById("cartCount");
+
+
+    const totalQuantity =
+        cart.reduce(
+            (total, item) =>
+                total + item.quantity,
+            0
+        );
+
+
+    cartCount.textContent = totalQuantity;
+
+    displayCart();
+}
+
+
+// =====================================================
+// DISPLAY CART
+// =====================================================
+
+function displayCart() {
+
+    const cartItems =
+        document.getElementById("cartItems");
+
+    const cartTotal =
+        document.getElementById("cartTotal");
+
+
+    cartItems.innerHTML = "";
+
+
+    if (cart.length === 0) {
+
+        cartItems.innerHTML =
+            "<p>Your cart is empty 🛒</p>";
+
+        cartTotal.textContent = "0";
+
+        return;
+    }
+
+
+    let total = 0;
+
+
+    cart.forEach(item => {
+
+        total +=
+            item.price * item.quantity;
+
+
+        const cartItem =
+            document.createElement("div");
+
+
+        cartItem.className =
+            "cart-item";
+
+
+        cartItem.innerHTML = `
+
+            <div>
+
+                <strong>
+                    ${item.name}
+                </strong>
+
+                <p>
+                    ₹${item.price.toLocaleString("en-IN")}
+                </p>
+
+            </div>
+
+            <div class="quantity">
+
+                <button
+                    onclick="changeQuantity(${item.id}, -1)">
+                    -
+                </button>
+
+                <span>
+                    ${item.quantity}
+                </span>
+
+                <button
+                    onclick="changeQuantity(${item.id}, 1)">
+                    +
+                </button>
+
+            </div>
+
+            <button
+                class="remove-btn"
+                onclick="removeFromCart(${item.id})">
+
+                ❌
+
+            </button>
+        `;
+
+
+        cartItems.appendChild(cartItem);
+
+    });
+
+
+    cartTotal.textContent =
+        total.toLocaleString("en-IN");
+}
+
+
+// =====================================================
+// CHANGE QUANTITY
+// =====================================================
+
+function changeQuantity(id, change) {
+
+    const item =
+        cart.find(pet => pet.id === id);
+
+
+    if (!item) {
+        return;
+    }
+
+
+    item.quantity += change;
+
+
+    if (item.quantity <= 0) {
+
+        cart =
+            cart.filter(pet => pet.id !== id);
+
+    }
+
+
+    updateCart();
+}
+
+
+// =====================================================
+// REMOVE FROM CART
+// =====================================================
+
+function removeFromCart(id) {
+
+    cart =
+        cart.filter(pet => pet.id !== id);
+
+    updateCart();
+}
+
+
+// =====================================================
+// CART MODAL
+// =====================================================
+
+function openCart() {
+
+    document.getElementById("cartModal")
+        .style.display = "flex";
+
+    displayCart();
+}
+
+
+function closeCart() {
+
+    document.getElementById("cartModal")
+        .style.display = "none";
+}
+
+
+// =====================================================
+// ORDER FORM
+// =====================================================
+
+function openOrderForm() {
+
+    if (cart.length === 0) {
+
+        alert("Please add a pet to cart first.");
+
+        return;
+    }
+
+
+    document.getElementById("orderModal")
+        .style.display = "flex";
+}
+
+
+function closeOrderForm() {
+
+    document.getElementById("orderModal")
+        .style.display = "none";
+}
+
+
+// =====================================================
+// PLACE ORDER
+// =====================================================
+
+function placeOrder(event) {
+
+    event.preventDefault();
+
+
+    const name =
+        document.getElementById("customerName").value;
+
+
+    alert(
+        `Thank you ${name}! 🎉\n\n` +
+        `Your PetNest order has been placed successfully.\n\n` +
+        `Payment: Cash on Delivery`
+    );
+
+
+    cart = [];
+
+    updateCart();
+
+    closeOrderForm();
+
+    closeCart();
+}
+
+
+// =====================================================
+// CLOSE MODAL OUTSIDE CLICK
+// =====================================================
+
+window.onclick = function(event) {
+
+    const cartModal =
+        document.getElementById("cartModal");
+
+    const orderModal =
+        document.getElementById("orderModal");
+
+
+    if (event.target === cartModal) {
+        closeCart();
+    }
+
+
+    if (event.target === orderModal) {
+        closeOrderForm();
+    }
+
+};
+
+
+// =====================================================
+// PAGE LOAD
+// =====================================================
+
+displayPets(pets);
+
+updateCart();
